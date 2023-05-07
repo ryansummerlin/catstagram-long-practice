8489c5a29663d1433af21b65fb6f0a0b996e8592
ryansummerlin <ryansummerlin777@gmail.com>
2023-05-07 13:37:12 -0700

    Finished phase 1

--- public/index.js
+++ public/index.js
@@ -1,1 +1,25 @@
-// Your code here
+const catImageURL = "https://api.thecatapi.com/v1/images/search";
+
+const getCatImage = function() {
+
+    fetch(catImageURL)
+    .then(function(res) {
+        return res.text();
+      })
+      .then(function(data) {
+        let parsedData = JSON.parse(data);
+        return parsedData[0]
+      })
+      .then(function(obj) {
+        const imageContainer = document.getElementById("image-container");
+        const image = document.createElement("img");
+        image.setAttribute("id", "image");
+        image.setAttribute("alt", "cat image from Cat API");
+        let imageURL = obj["url"];
+        image.setAttribute("src", imageURL);
+        imageContainer.appendChild(image);
+      });
+
+}
+
+document.addEventListener("DOMContentLoaded", getCatImage);
